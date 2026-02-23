d4131dd82a82fda1cde2d5a952a7ded600791fbe
Aditya Kalbhor <kalbhoraditya4126@gmail.com>
2026-02-23 22:25:58 +0530

    Added GST and total items feature

--- General Store Billing system.py
+++ General Store Billing system.py
@@ -1,11 +1,11 @@
-print("======== ALL IN ONE GENERAL STORE ========")
+print("         ALL IN ONE GENERAL STORE ")
 print("------------------------------------------")
 customer_name = input("Enter customer name: ")
 print("\n------------------ ITEMS -----------------")
 
 basket = []
 grand_total = 0
-
+total_items = 0
 while True:
     item_name = input("Enter the item name: ")
     item_quantity = int(input("Enter the quantity: "))
@@ -13,32 +13,32 @@ while True:
 
     final_price = item_quantity * product_price
 
-    print("------------------------------------------")
     print(f"Final price: {final_price}")
     print("------------------------------------------")
 
-    # add to grand total
-    grand_total += final_price
-
+    grand_total += final_price  # add to grand total
+    total_items += item_quantity  # add to total items
     # store item in basket
-    basket.append({
+    basket.append({      
         "name": item_name,
         "quantity": item_quantity,
         "price": product_price,
         "total": final_price
     })
-
     choice = input("Do you want to add more item (y/n): ")
-
     if choice.lower() != 'y':
         break
-
 print("\n================= BILL ===================")
 
 for item in basket:
     print(f"{item['name']} x {item['quantity']} = ₹{item['total']}")
+print("\nTotal items purchased:",total_items)
 
 print("------------------------------------------")
 print("Customer name:", customer_name)
-print("Grand Total : ₹",grand_total)
+gst = grand_total * 0.18
+final_amount = grand_total + gst
+print("Subtotal: ₹",grand_total)
+print("------------------------------------------")
+print("Total Amount (Incl. GST): ₹",final_amount)
 print("------------------------------------------")
