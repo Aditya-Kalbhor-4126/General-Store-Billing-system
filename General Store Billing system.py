56344856b24c385241c1bd859a666ecfd3920d86
Aditya Kalbhor <kalbhoraditya4126@gmail.com>
2026-02-25 20:26:44 +0530

    Add input validation and fix indentation issue

--- General Store Billing system.py
+++ General Store Billing system.py
@@ -9,41 +9,71 @@ now = datetime.datetime.now()
 basket = []
 grand_total = 0
 total_items = 0
+
 while True:
-    item_name = input("Enter the item name: ")
-    item_quantity = int(input("Enter the quantity: "))
-    product_price = int(input("Enter the price: "))
+    while True:
+        item_name = input("Enter item name: ")
+        if item_name.replace(" ", "").isalpha():
+            break
+        print("Enter valid item name.")
+
+    while True:
+        try:
+            item_quantity = int(input("Enter quantity: "))
+            break
+        except ValueError:
+            print("Enter numbers only.")
+
+    while True:
+        try:
+            product_price = float(input("Enter price: "))
+            break
+        except ValueError:
+            print("Enter valid price.")
 
     final_price = item_quantity * product_price
 
     print(f"Final price: {final_price}")
     print("------------------------------------------")
 
-    grand_total += final_price  # add to grand total
-    total_items += item_quantity  # add to total items
-    # store item in basket
-    basket.append({      
+    grand_total += final_price
+    total_items += item_quantity
+
+    basket.append({
         "name": item_name,
         "quantity": item_quantity,
         "price": product_price,
         "total": final_price
     })
-    choice = input("Do you want to add more item (y/n): ")
-    if choice.lower() != 'y':
+
+    while True:
+        choice = input("Do you want to add more item (y/n): ").lower()
+        if choice == 'y':
+            break
+        elif choice == 'n':
+            break
+        else:
+            print("Please enter only y or n.")
+
+    if choice == 'n':
         break
+    
 print("\n================= BILL ===================")
 
 for item in basket:
     print(f"{item['name']} x {item['quantity']} = ₹{item['total']}")
-print("\nTotal items purchased:",total_items)
+
+print("\nTotal items purchased:", total_items)
 
 print("------------------------------------------")
 print("Customer name:", customer_name)
-print("Date:",now.strftime("%d-%m-%Y"))
-print("Time:",now.strftime("%H:%M:%S"))
+print("Date:", now.strftime("%d-%m-%Y"))
+print("Time:", now.strftime("%H:%M:%S"))
+
 gst = grand_total * 0.18
 final_amount = grand_total + gst
-print("Subtotal: ₹",grand_total)
+
+print("Subtotal: ₹", grand_total)
 print("------------------------------------------")
-print("Total Amount (Incl. GST): ₹",final_amount)
+print("Total Amount (Incl. GST): ₹", final_amount)
 print("------------------------------------------")
